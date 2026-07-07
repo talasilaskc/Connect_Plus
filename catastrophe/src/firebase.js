fe59ab0d624fb66e7ae334b02f560dad07348b16
talasilaskc <152628894+talasilaskc@users.noreply.github.com>
2026-07-07 20:42:14 +0530

    Replace Firebase config values with placeholders
    Updated Firebase configuration to use placeholders for sensitive information.

--- catastrophe/src/firebase.js
+++ catastrophe/src/firebase.js
@@ -8,13 +8,13 @@ import {getStorage} from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCY4J1d9GF4aKDP6TUlgTa14_ieRPih08Y",
-  authDomain: "fieldproject-ecab7.firebaseapp.com",
-  projectId: "fieldproject-ecab7",
-  storageBucket: "fieldproject-ecab7.appspot.com",
-  messagingSenderId: "310840282684",
-  appId: "1:310840282684:web:d6c26367dda57afde6bd39",
-  measurementId: "G-94KKTFVZCC"
+  apiKey: "....",
+  authDomain: "....",
+  projectId: "....",
+  storageBucket: ".....",
+  messagingSenderId: ".....",
+  appId: ".......",
+  measurementId: "......"
 };
 
 // Initialize Firebase
